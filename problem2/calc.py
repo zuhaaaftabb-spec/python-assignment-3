c68d223cdf9fb2727526334023be34c264f022ce
zuhaaaftabb-spec <zuhaa.aftabb@gmail.com>
2025-12-09 21:58:48 +0500

    Implement multiply function in calc.py

---
+++ problem2/calc.py
@@ -0,0 +1,13 @@
+def multiply(a, b):
+    """
+    Multiply two numbers.
+    
+    Args:
+        a: First number
+        b: Second number
+    
+    Returns:
+        Product of a and b
+    """
+    return a * b
+
